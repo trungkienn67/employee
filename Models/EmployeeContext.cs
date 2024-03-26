e574ded1b343729a09a75fd445768e44efe065f9
trungkienn67 <153173408+trungkienn67@users.noreply.github.com>
2024-03-26 10:02:57 +0700

    Add files via upload

---
+++ Models/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using HRM.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRM.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; } = null!;
+    }
+}
